c2c500205e7778a75b8a66f61b5514d08eefba6e
Michael Hunger <github@jexp.de>
2016-03-18 12:15:14 +0100

    Updated bolt driver to M05, Neo4j to 3.0.0-SNAPSHOT

--- src/test/java/example/LegacyFullTextIndexTest.java
+++ src/test/java/example/LegacyFullTextIndexTest.java
@@ -2,17 +2,12 @@ package example;
 
 import org.junit.Rule;
 import org.junit.Test;
-
-import org.neo4j.driver.v1.Driver;
-import org.neo4j.driver.v1.GraphDatabase;
-import org.neo4j.driver.v1.ResultCursor;
-import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.*;
+import org.neo4j.graphdb.factory.GraphDatabaseSettings;
 import org.neo4j.harness.junit.Neo4jRule;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.neo4j.bolt.BoltKernelExtension.Settings.connector;
-import static org.neo4j.bolt.BoltKernelExtension.Settings.enabled;
+import static org.junit.Assert.assertThat;
 import static org.neo4j.driver.v1.Values.parameters;
 
 public class LegacyFullTextIndexTest
@@ -25,13 +20,14 @@ public class LegacyFullTextIndexTest
             .withProcedure( FullTextIndex.class )
 
             // Temporary until Neo4jRule includes Bolt by default
-            .withConfig( connector( 0, enabled ), "true" );
+            .withConfig(GraphDatabaseSettings.boltConnector("0").enabled, "TRUE" )
+            .withConfig(GraphDatabaseSettings.boltConnector("0").encryption_level, "OPTIONAL" );
 
     @Test
     public void shouldAllowIndexingAndFindingANode() throws Throwable
     {
         // In a try-block, to make sure we close the driver after the test
-        try( Driver driver = GraphDatabase.driver( "bolt://localhost" ) )
+        try( Driver driver = GraphDatabase.driver( neo4j.boltURI() , Config.build().withEncryptionLevel( Config.EncryptionLevel.NONE ).toConfig() ) )
         {
 
             // Given I've started Neo4j with the FullTextIndex procedure class
@@ -47,7 +43,7 @@ public class LegacyFullTextIndexTest
             session.run( "CALL example.index({id}, ['name'])", parameters( "id", nodeId ) );
 
             // Then I can search for that node with lucene query syntax
-            ResultCursor result = session.run( "CALL example.search('User', 'name:Brook*')" );
+            StatementResult result = session.run( "CALL example.search('User', 'name:Brook*')" );
             assertThat( result.single().get( "nodeId" ).asLong(), equalTo( nodeId ) );
         }
     }
